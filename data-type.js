ab8914c5de2aaa7415392d23a1c940f2baebbbc1
Monir1231 <monirmolla324@gmail.com>
2025-06-05 20:59:57 +0600

    javascript

---
+++ data-type.js
@@ -0,0 +1,48 @@
+// string
+let name = "Monir";
+console.log(name)
+console.log(typeof name)
+
+// number type 
+
+let number = 123;
+console.log(number)
+console.log(typeof number)
+
+
+// float type 
+
+let float = 123;
+console.log(float)
+console.log(typeof float)
+
+
+// float type 
+
+let empty = null;
+console.log(empty)
+console.log(typeof empty)
+
+
+// boolean type
+
+
+let yes = true
+console.log(typeof yes)
+
+let no = false
+console.log(typeof no)
+
+let error = 'error';
+let hasError = Boolean(error);
+
+console.log( typeof hasError);
+age = 12
+
+
+// condation 
+if( age => 14){
+    console.log("You are big")
+}else{
+    console.log("you are so small")
+}
